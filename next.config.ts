e0ddb7b8cedf8cece9c3acbd554bd5e0fd570dba
Waqas Ali <sawaldherwa@gmail.com>
2024-11-12 18:03:40 +0500

    ignore eslint during build

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
